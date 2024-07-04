1c85b4f856f96a1ced778ddd6a26425f66a3c682
JeansAthiwat <athiwat5146@gmail.com>
2024-07-04 19:24:22 +0700

    ye

--- util/utils.py
+++ util/utils.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 
 
 def compute_label_difference(label1, label2):
-    # Calculate label difference as described in your code
     greater_than = (label1 > label2).float() * 0
     equal_to = (label1 == label2).float() * 1
     less_than = (label1 < label2).float() * 2
